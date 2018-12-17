88973a9d95a2c8cc003616e9419b53cc49257762
Jose Luis <jlluengoramos@gmail.com>
2018-12-17 23:03:39 +0100

    Se añade background animado,

---
+++ VDPet/Assets/Scripts/backgroundAnimated/animatedBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatedBG : MonoBehaviour
+{
+
+    public float speed = 1f; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        moveImage();
+    }
+
+void moveImage(){
+    transform.Translate (-speed * Time.deltaTime,0,0); 
+}
+}
