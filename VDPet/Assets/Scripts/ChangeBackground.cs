3044ea5c19276b06b7fdf2d67721ba5ee51391c5
David Bermejo <dbermejosimon@gmail.com>
2019-03-05 18:06:30 +0100

    Comienzo de implementación apagar la luz y mandar a dormir al pet

---
+++ VDPet/Assets/Scripts/ChangeBackground.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBackground : MonoBehaviour
+{
+
+    public GameObject quad;
+    public Texture day;
+    public Texture night;
+    public InteractiveElement button;
+
+    private Renderer rend;
+    public bool isDay;
+
+    void Update()
+    {
+        if (button.pressed){
+            //el control de flujo inferior asegura según el valor del booleano el cambio de textura en el renderer
+            if (isDay)
+            {
+                isDay = false;
+                rend.material.mainTexture = day;
+            }
+            else
+            {
+                isDay = true;
+                rend.material.mainTexture = night;
+            }
+            button.pressed = false;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDay = true;
+        this.rend = quad.GetComponent<Renderer>();
+    }
+}
---
+++ VDPet/Assets/Scripts/PetSleeping.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetSleeping : MonoBehaviour
+{
+    private Animator anim;
+    public InteractiveElement lightButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightButton.pressed)
+        {
+            anim.SetBool("isSleep", true);
+        }
+        else
+        {
+            anim.SetBool("isSleep", false);
+        }
+        
+    }
+}
