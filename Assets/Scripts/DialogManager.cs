2c375cc53785298e758291c6782e30fbc5a44388
David Bermejo <dbermejosimon@gmail.com>
2019-03-08 03:04:33 +0100

    Comienzo de implementación boton de comida y vitaminas

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour { 
+
+    public bool dialogOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FoodDialog.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodDialog : MonoBehaviour
+{
+
+
+    public InteractiveElement button;
+    public GameObject panel;
+    public InteractiveElement buttonMeal;
+    public InteractiveElement buttonVitamin;
+
+
+    private bool dialogOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogOpen = false;
+        panel.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.pressed && !dialogOpen)
+        {
+           button.pressed = false;
+            dialogOpen = true;
+           panel.SetActive(true);
+           
+        }else if(button.pressed && dialogOpen)
+        {
+            button.pressed = false;
+            dialogOpen = false;
+            panel.SetActive(false);
+        }
+    }
+}
